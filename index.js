4abdbb554686eb4ecca82468b6d02e75ea19569b
suyog sinnarkar <suyogsinnarkar10@gmail.com>
2023-11-02 22:46:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+const port = 3000;
+
+const API_KEY = process.env.API_KEY;
+app.get('/', (req, res) => {
+const address = 'mumbai';
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${address}&units=metric&appid=${API_KEY}`;
+console.log(url)
+axios.get(url)
+    .then(response => {
+      const data = response.data;
+      const cityName = data.name;
+      const temperature = data.main.temp;
+      const sunsetTime = new Date(data.sys.sunset * 1000).toLocaleTimeString();
+      const message = `City Name: ${cityName}<br>Temperature: ${temperature}&deg;C<br>Sunset Time: ${sunsetTime}`;
+
+      res.send(`<html><body><div id='container'><h1>${message}</h1></div></body></html>`);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('Error  while fetching weather data');
+    });
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
